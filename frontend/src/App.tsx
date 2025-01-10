cfddbcf39c3d58e84a8b9ac02a7f4aedfc72a5a4
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2025-01-10 08:08:06 +0530

    added backend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import NavBar from "./components/NavBar"
+import PostCard from "./components/PostCard"
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <PostCard />
+    </div>
+  )
+}
+
+export default App
