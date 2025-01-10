bf1c227dddbd0a32a85b145d75b1ce0db69a671b
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2025-01-10 06:41:12 +0530

    added post-card

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import NavBar from "./components/NavBar"
+import PostCard from "./components/PostCard"
 
 function App() {
   return (
     <div>
       <NavBar />
+      <PostCard />
       hello
     </div>
   )
